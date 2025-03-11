0d2293fa58e8678a387164d06f1f8017ed5a4096
aliasgirl <72706635+alias-girl@users.noreply.github.com>
2025-03-11 12:23:02 -0400

    Major Changes (03/11/2025)
    > Created sub-page functionality (technically different pages viewable from the "same" page, but I don't know what else to call it)
            - Standardized the layout for all of the "About Us" sub-pages
                - None of the "About Us" sub-pages have any content. I should probably add that asap
                - All of the sub-pages themselves use custom, less distracting/intense opening animations
            - Pretty much finished with "Visit Us" and all of the sub-pages
                - Only like 3 of the 6 or so websites on the resources page are actually still registered. I should ask Guy about those
    
    > Changed the warning to take up more of the user's attention upon opening the main page of the website
            - I'm not so sure about this one. It might seem pretty scary to open a website and be told that it's still being worked on, so literally anybody who cares anymore please tell me if you think this is a good idea or not. If I don't get any answers within a month, then I will know how you all truly feel about me and my work. (I won't really care that much)
            - It's only for the main page, so you won't have to deal with it on every page.
    
    > TO BE DONE:
            - A lot.
            - Make a suitable background image loop of some kind to put on every page, preferably one that isn't a billion terabytes (or custom background images for all of them if I want to hurt my brain more than I already am)
            - Update information on all of the About Us sub-pages
            - Create all of the standard sub-pages for any other pages that need them
            - Start caring about my life (can be put off for longer if needed)

---
+++ SRRL UPDATED 03-11-2025/anim-delay.js
@@ -0,0 +1,11 @@
+const mainDoc = "Sandy River & Rangeley Lakes RR"
+const mainNav = document.getElementById("navbar")
+
+function animDelay() {
+    if (document.title = mainDoc) {
+        mainNav.style.animationDelay = "6s";
+    }
+    else {
+        return;
+    }
+}
